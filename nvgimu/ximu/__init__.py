363d99d44a82ba5819529852b1bdf2e01a6ccc02
alfkjartan <kjartan.halvorsen@gmail.com>
2014-03-28 15:49:24 +0100

    Added __init__.py for ximu package

---
+++ nvgimu/ximu/__init__.py
@@ -0,0 +1,18 @@
+"""
+Code for working with ximu data
+"""
+# Copyright (C) 2009-2011 Kjartan Halvorsen
+#
+#
+# This is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# IMUSim is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this software.  If not, see <http://www.gnu.org/licenses/>.
